316ab2da32248591bc8c798daff48fae855549ec
ayon121 <borshonsaha1234@gmail.com>
2023-10-20 14:50:38 +0600

    add to cart funtion done

--- src/Components/Product/ProductDetails.jsx
+++ src/Components/Product/ProductDetails.jsx
@@ -1,35 +1,60 @@
 
 // import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Navbar from '../Shared/Navbar';
-
+import { ToastContainer } from 'react-toastify';
+import {  toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthContext } from '../../Providers/AuthProviers';
 const ProductDetails = ()=> {
     const { id } = useParams()
     const [currentproduct, setProduct] = useState([])
-
+    const { user } = useContext(AuthContext)
     useEffect(() => {
         fetch(`http://localhost:5000/product/${id}`)
             .then(res => res.json())
             .then(data => setProduct(data))
     }, [id])
-
     const { name, price, image, description, category, brands  } = currentproduct
+    const email = user.email
+    const Cart = {...currentproduct , email}
+
+    const addtocart = (Cart) => {
+        fetch('http://localhost:5000/Mycart', 
+           {
+            method : 'POST',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(Cart)
+           } 
+        )
+        .then(res => res.json())
+        .then(data => {
+            if(data.insertedId){
+                toast("Product Added Successfully")
+            
+            }
+        })
+    }
+    
     return (
         <div>
             <div className='h-44'>
             <Navbar></Navbar>
-            </div>
+            </div >
             <h1 className='text-4xl text-center mt-4 font-Script'>{name}</h1>
-            <div className='max-w-5xl mx-auto mt-4 border-black border-4 rounded-3xl h-96'>
+            <div className='max-w-5xl mx-auto mt-4 px-3 border-black border-4 rounded-3xl h-96'>
                 <img className='w-full h-full rounded-3xl' src={image} alt="" />
             </div>
-            <div className='max-w-5xl mx-auto mt-4 font-Mukta'>
+            <div className='max-w-5xl mx-auto px-3 mt-4 font-Mukta'>
                 <p>{description}</p>
                 <p className='text-2xl font-bold mt-3'>{category} , {brands}</p>
                 <h1 className='text-3xl md:text-5xl mt-3'>${price}</h1>
-                <button className="btn mt-3 bg-black text-white">Add to Cart</button>
+                <button onClick={() => addtocart(Cart)} className="btn mt-3 bg-black text-white">Add to Cart</button>
             </div>
+            <ToastContainer />
         </div>
     );
 };
